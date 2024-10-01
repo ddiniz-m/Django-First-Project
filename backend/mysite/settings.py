64532098237240b1a796f2f40163508d292ec035
ddiniz-m <diogodinizm@gmail.com>
2024-10-01 13:42:52 +0100

    Added Bootstrap framework.

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangomysite.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
